1ab398d206108cb99ce72001f28c525db85506e4
mahadev mandal <mandalmahadev5@gmail.com>
2022-08-16 11:53:25 +0545

    mongo url chnaged

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,4 +1,5 @@
 export const baseURL = process.env.NODE_ENV === 'production' ? "https://deal-ayo.vercel.app" : "http://localhost:3000";
-export const mongoDbUri ='mongodb+srv://dealAyo:Dealayo123@cluster0.kqwi8q2.mongodb.net/?retryWrites=true&w=majority'
+// export const mongoDbUri ='mongodb+srv://dealAyo:Dealayo123@cluster0.kqwi8q2.mongodb.net/?retryWrites=true&w=majority'
+export const mongoDbUri ='mongodb+srv://kbcqna:8A3XxOKAf3ivjQNs@cluster0.hsnba.mongodb.net/kbcqna?retryWrites=true&w=majority'
 export const containerPadding = '0 50px';
 export const containerMargin = '0 50px';
