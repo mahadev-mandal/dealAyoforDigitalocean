a13c2506e34bcd04e360e372bfd789bd8cd1b80f
mahadev mandal <mandalmahadev5@gmail.com>
2022-08-16 13:21:45 +0545

    alll

--- pages/mark-attendance/index.js
+++ pages/mark-attendance/index.js
@@ -27,21 +27,22 @@ function Today() {
 
   let data;
   let d;
-
   // MERGE emp details and attendance data
-  if (attendance.length > 0) {
-    d = empDetails.map(emp => ({ ...emp, ...attendance[0].employees.find(attend => attend.dealAyoId === emp.dealAyoId) }));
-    if (parseJwt(Cookies.get('token')).role === 'admin' || parseJwt(Cookies.get('token')).role === 'super-admin') {
-      data = d
-    }
-    // Only make able to mark his/her attendance if he/she is not admin or super-admin
-    else {
-      data = d.filter(e => e.dealAyoId === parseJwt(Cookies.get('token')).dealAyoId)
+  if (attendance) {
+    if (attendance.length > 0) {
+      d = empDetails.map(emp => ({ ...emp, ...attendance[0].employees.find(attend => attend.dealAyoId === emp.dealAyoId) }));
+      if (parseJwt(Cookies.get('token')).role === 'admin' || parseJwt(Cookies.get('token')).role === 'super-admin') {
+        data = d
+      }
+      // Only make able to mark his/her attendance if he/she is not admin or super-admin
+      else {
+        data = d.filter(e => e.dealAyoId === parseJwt(Cookies.get('token')).dealAyoId)
+      }
     }
   } else {
     data = empDetails
   }
-  
+
   if (error1 || error2) {
     return <div>Failed to load employees details</div>
   } else if ((!data)) {
