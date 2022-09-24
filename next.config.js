0ec225e9be4897c561d43aeb9e3eb0b076b8657f
mahadev mandal <mandalmahadev5@gmail.com>
2022-09-24 14:44:52 +0545

    pwa1

--- next.config.js
+++ next.config.js
@@ -1,23 +1,43 @@
+// /** @type {import('next').NextConfig} */
+
+// const withPWA = require("next-pwa");
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   eslint: {
+//     dirs: ['pages', 'utils'],
+//   },
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     // disable: process.env.NODE_ENV === 'development',
+//     skipWaiting: true,
+//   },
+// }
+
+// module.exports = withPWA(nextConfig);
+
+// // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+
+
 /** @type {import('next').NextConfig} */
-
-const withPWA = require("next-pwa");
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+}
+
+module.exports = nextConfig
+const withPWA = require("next-pwa");
+module.exports = withPWA({
   eslint: {
     dirs: ['pages', 'utils'],
   },
   pwa: {
     dest: "public",
     register: true,
-    // disable: process.env.NODE_ENV === 'development',
+    disable: process.env.NODE_ENV === 'development',
     skipWaiting: true,
   },
-}
-
-module.exports = withPWA(nextConfig);
-
-// Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-
-
+});
