b3de6e8ce202c80087a3fa59ecab5a7ebdd35d3b
mahadev mandal <mandalmahadev5@gmail.com>
2022-09-24 14:06:00 +0545

    pwa

--- next.config.js
+++ next.config.js
@@ -1,26 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
 
 const withPWA = require("next-pwa");
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-});
-
-// Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-module.exports = {
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   eslint: {
     dirs: ['pages', 'utils'],
   },
+  pwa: {
+    dest: "public",
+    register: true,
+    // disable: process.env.NODE_ENV === 'development',
+    skipWaiting: true,
+  },
 }
+
+module.exports = withPWA(nextConfig);
+
+// Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+
 
